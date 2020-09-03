e1ca48923d9fcfb13a156b6476fe29637f71f731
2014c2g1 <40223124@gm.nfu.edu.tw>
2020-09-03 22:35:59 +0800

    csvArduinoStore

---
+++ 6-CSV/csvArduinoStore.py
@@ -0,0 +1,42 @@
+import csv
+import pyfirmata
+from time import sleep
+port ='com3'
+board = pyfirmata.Arduino(port)
+
+it  = pyfirmata.util.Iterator(board)
+it.start()
+
+pirPin = board.get_pin('d:11:i')
+a0 = board.get_pin('a:0:i')
+
+with open ('SensorDataStore.csv','w') as f:
+	w = csv.writer(f)
+	w.writerow(["Number","Potentiometer","Motion sensor"])
+	i = 0
+	sleep(1)
+	pirData = pirPin.read()
+	potData = a0.read()
+	print("begin")
+	while i < 25:
+		sleep(1)
+		pirData = pirPin.read()
+		potData = a0.read()
+		if potData is not None:
+			i += 1
+			row = [i ,potData,pirData]
+			w.writerow(row)
+			print(i)
+			print(potData)
+			print(pirData)
+		else:
+			print("sonor is alarm")
+			i += 1
+			row = [i ,potData,pirData]
+			w.writerow(row)
+			print(i)
+			print(potData)
+			print(pirData)
+print("Done. CSV file is ready")
+sleep(3)
+board.exit()
